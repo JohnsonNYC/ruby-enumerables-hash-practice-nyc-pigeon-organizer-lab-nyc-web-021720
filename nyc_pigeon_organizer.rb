10992ea15301e612c040f5435f37ce0ac7be7a43
Johnson Kow <jkow95@gmail.com>
2020-02-11 02:54:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   newHash = {}
-  data.each {|key,value|              #:color => :purple
-    value.each {|characteristic,name| # :purple => name
-      name.each {|name|   #Branching through data to access names
+  data.each do |key,value|              #:color => :purple
+    value.each do |characteristic,name| # :purple => name
+      name.each do |name|   #Branching through data to access names
   
   if !newHash[name]       # If newHash = {name: []} DNE 
     newHash[name] = {}    # It is created 
@@ -13,9 +13,9 @@ def nyc_pigeon_organizer(data)
   end 
   
   newHash[name][key] << characteristic.to_s #turn charactistic key                                         #to string 
-      }
-    }
-  }
+    end 
+  end 
+end 
   
  newHash
 
